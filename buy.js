13c38ae5e833d1825db3cb9b4545de504a18b62c
VNC <ngocchuong90@gmail.com>
2022-10-18 16:18:45 +0700

    get Pair Infor

--- buy.js
+++ buy.js
@@ -5,7 +5,7 @@ import {ABI_FACTORY, ABI_MINIMUM, ABI_PAIR, ABI_ROUNTER,ADD_ROUNTER,ADD_FACTORY,
 let PRIVATEKEY = '532e9f22cc162df0eac45efb7be291d35710c6b0b5af9638903d1ce04059797d'
 PRIVATEKEY = fs.readFileSync('./driver.secrec').toString().trim()
 //setup to buy
-let TOKEN_ADDRESS = '0x9aab0a9b3a2f7cf669f1385c48e0a063b93834bb'
+let TOKEN_ADDRESS = '0x960a69E979d2F507E80121f41d984Ea8aD83cD76'
 const AMOUT_BNB_BUY = 0.001 // BNB buy
 const SLIPPAGE_BUY = 5 //5%
 const GAS_PRICE = 5 // Wei
@@ -36,24 +36,55 @@ const getPairInfor = async (contractFactory)  => {
     const contractUSDC = new ethers.Contract(USDC, ABI_MINIMUM, provider)
     const pairBNB = await contractFactory.getPair(TOKEN_ADDRESS, BNB)
     const valuePairBNB = await contractBNB.balanceOf(pairBNB)
-    console.log(valuePairBNB);
     const valuePairBNBatUSDT = (await contractRounter.getAmountsOut(valuePairBNB, [BNB,USDT]))[1]
-    console.log(valuePairBNBatUSDT);
     const pairUSDT = await contractFactory.getPair(TOKEN_ADDRESS, USDT)
     const valuePairUSDT = await contractUSDT.balanceOf(pairUSDT)
     const pairBUSD = await contractFactory.getPair(TOKEN_ADDRESS, BUSD)
     const valuePairBUSD = await contractBUSD.balanceOf(pairBUSD)
     const pairUSDC = await contractFactory.getPair(TOKEN_ADDRESS, USDC)
     const valuePairUSDC = await contractUSDC.balanceOf(pairUSDC)
+    const pair = [
+        {
+            pair : 'BNB',
+            pairAddress : pairBNB,
+            value : valuePairBNBatUSDT
+        }, {
+            pair : 'USDT',
+            pairAddress : pairUSDT,
+            value : valuePairUSDT
+        }, {
+            pair : 'BNB',
+            pairAddress : pairBUSD,
+            value : valuePairBUSD
+        }, {
+            pair : 'USDC',
+            pairAddress : pairUSDC,
+            value : valuePairUSDC
+        }
+    ]
+    let max = 0
+    let index = 0
+    pair.forEach((pair,i)=>{
+        if( max < pair.value) {
+            max = pair.value
+            index = i
+        }
+    })
+    return pair[index]
 
     
 }
 //main
 const main = async () => {
-    getPairInfor(contractFactory)
+    const token = await getInforToken(contractToken)
+    const pair = await getPairInfor(contractFactory)
+    console.log(`
+    Ban dang mua token :${token.symbol}/ ${token.name}
+    Liquid Pool : ${pair.pair}
+    Pair Address : ${pair.pairAddress}
+    `);
 
 }
-
 
 main()
 
